2f075cfc163369d758d9fe710e8165e3ac89649f
otisarshdeep <arshtejay@gmail.com>
2023-03-18 20:42:21 -0300

    chore: update based on tutorial https://scrimba.com/learn/learnreact

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,10 +1,27 @@
+import "../style.css"
 
-function Sidebar() {
-    return (
-      <div>
-        <h1>Notes</h1>
+export default function Sidebar(props) {
+  const noteElements = props.notes.map((note, index) => (
+      <div key={note.id}>
+          <div
+              
+              className={`title ${
+                  note.id === props.currentNote.id ? "selected-note" : ""
+              }`}
+              onClick={() => props.setCurrentNoteId(note.id)}
+          >
+              <h4 className="text-snippet">Note {index + 1}</h4>
+          </div>
       </div>
-    );
-  }
-  
-  export default App;
+  ))
+
+  return (
+      <section className="pane sidebar">
+          <div className="sidebar--header">
+              <h3>Notes</h3>
+              <button className="new-note" onClick={props.newNote}>+</button>
+          </div>
+          {noteElements}
+      </section>
+  )
+}
